483bd90e58ddcf5162e80e2acb6d2315602a39a5
Emellie29 <emellie.lopez@udeo.edu.gt>
2025-07-21 17:18:02 -0600

    segunda parte

--- Tarea 8.py
+++ Tarea 8.py
@@ -1,20 +1,26 @@
 while True:
-    print("\n--- Menú Principal ---")
-    print("1. Ver estudiantes")
-    print("2. Añadir estudiante")
-    print("3. Salir")
+    print("\n♥♥♥♥♥♥♥♥MENÚ♥♥♥♥♥♥♥♥")
+    print("1. Factorial")
+    print("2. Suma")
+    print("3. Fibonacci")
+    print("4. Contar una letra en una palabra")
+    print("5. Invertir cadena de texto")
+    print("6. Potencia")
+    print("7. Salir")
+
     opcion = input("Selecciona una opción: ")
+
     match opcion:
-        case 1:
+        case "1":
             def factorial(n):
                 if n == 0:
                     return 1
                 else:
                     print(n)
-                return n * factorial(n - 1)
+                    return n * factorial(n - 1)
             n = int(input("Ingrese un número entero positivo: "))
             print(factorial(n))
-        case 2:
+        case "2":
             def suma_numeros(n):
                 if n == 1:
                     return 1
@@ -24,38 +30,52 @@ while True:
                 n = int(input("Ingresa un número entero positivo: "))
                 if n > 0:
                     resultado = suma_numeros(n)
-                    print(f"La suma de los primeros {n} números naturales es: {resultado}")
+                    print(f"La suma de los primeros {n} números es: {resultado}")
                 else:
                     print("Por favor ingresa un número mayor que cero.")
             except ValueError:
                 print("Entrada no válida. Debes ingresar un número entero.")
-        case 3:
+        case "3":
             def Fibonacci(n):
-                if n < 0:
+                if n<=1:
+                    return n
+                else:
+                    return Fibonacci(n-1) + Fibonacci(n-2)
+            n=int(input("Ingrese un número entero positivo: "))
+            print(f"Fibonacci({n}): {Fibonacci(n)}")
+        case "4":
+            def contar(palabra,letra,indice=0):
+                if indice == len(palabra):
                     return 0
-                elif n == 1:
-                    return 1
+                if palabra[indice].lower()== letra.lower():
+                    return contar(palabra,letra,indice+1)+1
                 else:
-                    return n + Fibonacci(n - 1) + Fibonacci(n - 2)
-                    n = int(input("Ingrese un numero entero positivo: "))
-                if n > 0:
-                    resultado = Fibonacci(n)
-                    print(f"La suma de {n}: {resultado}")
+                    return contar(palabra,letra,indice+1)
+            palabra=input("Ingrese una frase: ")
+            letra=input("Ingrese una letra para contar: ")
+            if len(letra)!=1:
+                print("Por favor ingresa una letra: ")
+            else:
+                cantidad=contar(palabra,letra)
+                print(f"la letra {letra} aparece {cantidad} veces en la frase {palabra}.")
+        case "5":
+            def cadena_Invertida(Cadena):
+                if len(Cadena)==0:
+                    return ""
                 else:
-                    print("Intente de nuevo")
-        case 4:
-            def
-        case 5:
-        case 6:
+                    return Cadena[-1]+cadena_Invertida(Cadena[:-1])
+            texto=input("Ingrese una cadena de texto: ")
+            invertir=cadena_Invertida(texto)
+            print(f"Cadena de texto invertida: {invertir}")
+        case "6":
             def potencia(base,exponente):
                 if exponente==0:
                     return 1
                 else:
-                    print(n)
                     return base*potencia(base,exponente-1)
             base=int(input("ingrese una base: "))
             exponente=int(input("ingrese un exponente: "))
             print(potencia(base,exponente))
-        case 7:
+        case "7":
             print("Saliendo del programa...")
             break
